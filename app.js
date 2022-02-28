f2c127fc1ddc693ca3ed3c343352dd807e9147ca
drnareshchauhan <drnareshchauhan@gmail.com>
2022-02-28 19:11:55 +0530

    online exam

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors(corsOptions));
 //passport
 app.use(passport.initialize());
 app.use(passport.session());
